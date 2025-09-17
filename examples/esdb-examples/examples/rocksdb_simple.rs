99a334e99a4b2bb236b9b160ffc328fd79671ee6
Andrew Cherry <andrew@xyncro.com>
2025-09-17 16:25:53 +0100

    experimenting

--- examples/esdb-examples/examples/rocksdb_simple.rs
+++ examples/esdb-examples/examples/rocksdb_simple.rs
@@ -1,15 +1,24 @@
 use std::error::Error;
 
 use rocksdb::{
+    ColumnFamilyDescriptor,
     DB,
     Options,
 };
 
 pub fn main() -> Result<(), Box<dyn Error>> {
-    let mut db = DB::open_default("./data/rocksdb/simple")?;
-    let () = db.create_cf("simple", &Options::default())?;
-    let cf = db.cf_handle("simple").unwrap();
+    let path = "./data/rocksdb/simple";
 
+    let cf_options = Options::default();
+    let cf = ColumnFamilyDescriptor::new("simple", cf_options);
+
+    let mut db_options = Options::default();
+    db_options.create_missing_column_families(true);
+    db_options.create_if_missing(true);
+
+    let db = DB::open_cf_descriptors(&db_options, path, [cf])?;
+
+    let cf = db.cf_handle("simple").expect("column family not found");
     let () = db.put_cf(&cf, b"key_a", b"value_a")?;
     let key_a = db.get_cf(&cf, b"key_a")?;
 
